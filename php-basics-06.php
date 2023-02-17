c0aa9cc47275794e64b590d435c1d74284b7f5f9
Youssef <youssefhamadi94@gmail.com>
2023-02-17 19:27:58 +0200

    php-basics-06.php

---
+++ php-basics-06.php
@@ -0,0 +1,11 @@
+<?php
+function calculateArea($heights, $width) {
+    if ($heights === 5 && $width === 5)
+           { echo "Area is 25!";
+           } else {
+             echo "Area is NOT 25";
+ } 
+}
+calculateArea(5,3)
+?>
+
