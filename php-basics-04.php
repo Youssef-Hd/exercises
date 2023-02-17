75d72cd1c354ef8f093c9c7d86a9d50f4c9117b1
Youssef <youssefhamadi94@gmail.com>
2023-02-17 17:26:25 +0200

    php-basics-04

---
+++ php-basics-04.php
@@ -0,0 +1,33 @@
+<?php
+$name = "Hello,My Name Is Youssef,";
+$position = "I am a developer, please check my github";
+$github_url = "https://github.com/Youssef-codi";
+
+echo "$name "  . $position .  "$github_url";
+?>
+
+<?php
+$name = "Hello,My Name Is Youssef,";
+$position = "I am a developer, please check my github";
+$github_url = "https://github.com/Youssef-codi";
+
+echo "$name $position $github_url";
+?>
+
+
+<?php
+$name = "Hello,My Name Is Youssef,";
+$position = "I am a developer, please check my github";
+$github_url = "https://github.com/Youssef-codi";
+
+echo $name .  $position  . $github_url;
+?>
+
+
+<?php
+$name = "Hello,My Name Is Youssef,";
+$position = "I am a developer, please check my github";
+$github_url = "https://github.com/Youssef-codi";
+
+print "$name $position $github_url";
+?>
