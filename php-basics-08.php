450b36485ab1915863d55359cf3944a1ecdb050e
Youssef <youssefhamadi94@gmail.com>
2023-02-18 08:58:36 +0200

    php-basic-08, data types array

---
+++ php-basics-08.php
@@ -0,0 +1,15 @@
+<?php
+$grocery = array("Eggs", "Milk", "Cheese", "Water Pack", "Tissues", "Watermelon");
+
+echo "Hello Sir,do you have $grocery[0], $grocery[1], and $grocery[2]? <br>"; 
+
+echo "Also if you sell fruits can i find a $grocery[5]? ";
+
+?>
+
+
+
+
+ <!-- Output the first 3 items of the array and the last item using the following message,
+ Hello Sir, do you have Eggs, Milk, and Cheese?
+ Also if you sell fruits can I find a Watermelon? -->
