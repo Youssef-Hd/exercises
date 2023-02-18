a841ccfc4b543ea49897301ef12ab579c64f13d9
Youssef <youssefhamadi94@gmail.com>
2023-02-18 14:04:24 +0200

    php-basics-02 step 1

---
+++ php-basics-02-01.php
@@ -0,0 +1,11 @@
+<!-- Write a PHP script to display the following strings.
+You can only use Echo or Print.
+Expected Output :
+Tomorrow I 'll learn PHP global variables.
+This is a bad command: sudo rm -rf / -->
+
+
+<?php
+echo "Tomorrow I'll learn PHP global variables.<br>";
+echo "This is a bad command: sudo -rf /";
+?>
