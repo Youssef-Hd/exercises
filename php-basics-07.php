268f939f013185d3628249e3c5a29decff619415
Youssef <youssefhamadi94@gmail.com>
2023-02-18 08:39:42 +0200

    php-basics-07

---
+++ php-basics-07.php
@@ -0,0 +1,11 @@
+<?php
+$birth_year = 1994;
+$current_year = 2023;
+
+if ($birth_year . $birth_year > 18) {
+    echo "You can drive";} 
+    
+    else {
+        echo "You still a kid, go and play GTA";
+    }
+?>
